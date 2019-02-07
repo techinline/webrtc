43c5bc8239efff560c2c574e23f8669b691410e2
Anton <ecspertiza@yandex.ru>
2019-02-07 14:02:44 +0300

    patch for webrtc 70

--- build.bat
+++ build.bat
@@ -4,10 +4,10 @@ SET buildMode=%1
 shift
 echo %buildMode%
 IF %buildMode%==release (
-	gn gen out/release_x86 "--args=is_debug=false target_cpu=\"x86\" rtc_include_tests=false is_clang=false"
+	gn gen out/release_x86 "--args=is_debug=false target_cpu=\"x86\" rtc_include_tests=false is_clang=false rtc_use_dummy_audio_file_devices=true"
 	ninja -C out/release_x86 -j 16
 )
 IF %buildMode%==debug (
-	gn gen out/debug_x86 "--args=is_debug=true target_cpu=\"x86\" rtc_include_tests=false is_clang=false"
+	gn gen out/debug_x86 "--args=is_debug=true target_cpu=\"x86\" rtc_include_tests=false is_clang=false rtc_use_dummy_audio_file_devices=true"
 	ninja -C out/debug_x86 -j 16
 )
