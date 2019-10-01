182b7f2e337c97477c7a56b7ef788f9d4985e172
dmitry.scheglev <sdv@pochtamt.ru>
2019-10-01 15:09:14 +0300

    * physical socket server fix

---
+++ apply_patch.bat
@@ -0,0 +1,20 @@
+echo off
+SET WEBRTC=%1
+set PATCH_DIR=%CD%
+echo "Webrtc root"%WEBRTC%
+echo "Patch folder:"%PATCH_DIR%
+
+pushd %WEBRTC%\src\
+cd build
+echo %CD%
+git.exe --work-tree %WEBRTC%\src\build apply %PATCH_DIR%/build.patch
+cd ..\third_party\usrsctp\usrsctplib\
+echo %CD%
+git.exe --work-tree %CD% apply %PATCH_DIR%/usrsctplib.patch
+cd ..\..\..\
+echo %CD%
+git.exe --work-tree %CD% apply %PATCH_DIR%/webrtc.patch
+git.exe --work-tree %CD% apply %PATCH_DIR%/webrtc-physical.patch
+popd
+
+
